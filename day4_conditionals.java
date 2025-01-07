b25764844abb08c03d9645ae5c5e2b7de73be431
kowshikreddy17 <kowshikreddyb29@gmail.com>
2025-01-07 20:49:26 +0530

    Add files via upload

---
+++ day4_conditionals.java
@@ -0,0 +1,9 @@
+public class day4_conditionals {
+    public static void main(String[] args) {
+        for(int i=20;i>=20 && i<=35;i++){
+            System.out.println(i);
+
+        }
+    }
+    
+}
